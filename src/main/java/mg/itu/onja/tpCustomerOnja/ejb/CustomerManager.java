21c9ce52bf8889dfd8d61653ebf47494ea84e447
Onja Tsiory <onjampandresenaTsiorinanahary@gmail.com>
2023-03-16 04:20:17 +0300

    Ajout EJB CustomerManager

--- src/main/java/mg/itu/onja/tpCustomerOnja/ejb/CustomerManager.java
+++ src/main/java/mg/itu/onja/tpCustomerOnja/ejb/CustomerManager.java
@@ -4,11 +4,30 @@
  */
 package mg.itu.onja.tpCustomerOnja.ejb;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import mg.itu.onja.tpCustomerOnja.entities.Customer;
 /**
  *
- * @author ELITEBOOK
+ * Gère la persistance des Customers.
  */
 @Stateless
 public class CustomerManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {  
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();  
+    }  
+        
+    public Customer update(Customer customer) {
+        return em.merge(customer);  
+    }   
     
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
 }
