2d3b8b998227f074bbc54efb628b30ec38c56d18
Onja Tsiory <onjampandresenaTsiorinanahary@gmail.com>
2023-03-16 03:58:30 +0300

    Création d'un EJB Session Bean Stateless CustomerManager pour la gestion des clients

---
+++ src/main/java/mg/itu/onja/tpCustomerOnja/ejb/CustomerManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.onja.tpCustomerOnja.ejb;
+import jakarta.ejb.Stateless;
+/**
+ *
+ * @author ELITEBOOK
+ */
+@Stateless
+public class CustomerManager {
+    
+}
