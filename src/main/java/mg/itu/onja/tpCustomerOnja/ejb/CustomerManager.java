e0a5b4b0516f1f29e42c5bbbec5286c9cbd3d56d
Onja Tsiory <onjampandresenaTsiorinanahary@gmail.com>
2023-03-24 16:58:14 +0300

    <h:outputText> => <h:link> && Ajout d'un backing bean pour la page des détails (CustomerDetailsBean)

--- src/main/java/mg/itu/onja/tpCustomerOnja/ejb/CustomerManager.java
+++ src/main/java/mg/itu/onja/tpCustomerOnja/ejb/CustomerManager.java
@@ -30,4 +30,8 @@ public class CustomerManager {
     public void persist(Customer customer) {
         em.persist(customer);
     }
+    
+    public Customer findById(int idCustomer){
+        return em.find(Customer.class, idCustomer);
+    }
 }
