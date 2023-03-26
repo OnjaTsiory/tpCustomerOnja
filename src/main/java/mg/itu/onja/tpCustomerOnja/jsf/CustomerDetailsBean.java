036b229c7d8ae48e69a56e7b7b8ce7c7bc89117b
Onja Tsiory <onjampandresenaTsiorinanahary@gmail.com>
2023-03-26 19:45:53 +0300

    Modification du backing bean de la page des détails

--- src/main/java/mg/itu/onja/tpCustomerOnja/jsf/CustomerDetailsBean.java
+++ src/main/java/mg/itu/onja/tpCustomerOnja/jsf/CustomerDetailsBean.java
@@ -9,7 +9,10 @@ import java.io.Serializable;
 import jakarta.ejb.EJB;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
+import java.util.List;
 import mg.itu.onja.tpCustomerOnja.ejb.CustomerManager;
+import mg.itu.onja.tpCustomerOnja.ejb.DiscountManager;
+import mg.itu.onja.tpCustomerOnja.entities.Discount;
 
 /**
  * Backing bean pour la page customerDetails.xhtml.
@@ -17,41 +20,52 @@ import mg.itu.onja.tpCustomerOnja.ejb.CustomerManager;
 @Named
 @ViewScoped
 public class CustomerDetailsBean implements Serializable {
-  private int idCustomer;
-  private Customer customer;
 
-  @EJB
-  private CustomerManager customerManager;
+    private int idCustomer;
+    private Customer customer;
+    private DiscountManager discountManager;
 
-  public int getIdCustomer() {
-    return idCustomer;
-  }
+    @EJB
+    private CustomerManager customerManager;
 
-  public void setIdCustomer(int idCustomer) {
-    this.idCustomer = idCustomer;
-  }
-
-  /**
-   * Retourne les détails du client courant (contenu dans l'attribut customer de
-   * cette classe).
-   */
-    public Customer getCustomer() {
-      return customer;
+    public int getIdCustomer() {
+        return idCustomer;
     }
 
-  /**
-   * Action handler - met à jour dans la base de données les données du client
-   * contenu dans la variable d'instance customer.
-   * @return la prochaine page à afficher, celle qui affiche la liste des clients.
-   */
-  public String update() {
-    // Modifie la base de données.
-    // Il faut affecter à customer (sera expliqué dans le cours).
-    customer = customerManager.update(customer);
-    return "customerList";
-  }
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
 
-  public void loadCustomer() {
-    this.customer = customerManager.findById(idCustomer);
-  }
+    /**
+     * Retourne les détails du client courant (contenu dans l'attribut customer
+     * de cette classe).
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Action handler - met à jour dans la base de données les données du client
+     * contenu dans la variable d'instance customer.
+     *
+     * @return la prochaine page à afficher, celle qui affiche la liste des
+     * clients.
+     */
+    public String update() {
+        // Modifie la base de données.
+        // Il faut affecter à customer (sera expliqué dans le cours).
+        customer = customerManager.update(customer);
+        return "customerList";
+    }
+
+    public void loadCustomer() {
+        this.customer = customerManager.findById(idCustomer);
+    }
+
+    /**
+     * Retourne la liste de tous les Discount.
+     */
+    public List<Discount> getDiscounts() {
+        return discountManager.getAllDiscounts();
+    }
 }
